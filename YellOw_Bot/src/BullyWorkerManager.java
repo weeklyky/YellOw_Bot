a664e516c1efa04368091874e4c958d3113eff2f
SDS <SDS@KYLE>
2017-06-30 17:40:35 +0900

    33

---
+++ YellOw_Bot/src/BullyWorkerManager.java
@@ -0,0 +1,13 @@
+import bwapi.Position;
+
+public class BullyWorkerManager {
+	
+	
+	boolean canBreakInto(){
+		//Position gate = InformationManager.Instance().getEnemyGate();
+		
+		return false;
+		
+	}
+
+}
